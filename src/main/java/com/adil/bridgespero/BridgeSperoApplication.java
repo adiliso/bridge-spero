46f2c0b798b55b05d40d215dcfe12d87fd45dd9b
Adil İsmayılov <ismayilovadil670@gmail.com>
2025-10-13 21:31:38 +0400

    Initial commit

---
+++ src/main/java/com/adil/bridgespero/BridgeSperoApplication.java
@@ -0,0 +1,13 @@
+package com.adil.bridgespero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BridgeSperoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BridgeSperoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adil/bridgespero/BridgeSperoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adil.bridgespero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BridgeSperoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
