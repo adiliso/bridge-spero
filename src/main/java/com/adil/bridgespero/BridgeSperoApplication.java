1e5898af5bd9d418108f073ad9dfadeb97ac979a
Adil İsmayılov <ismayilovadil670@gmail.com>
2025-10-27 15:22:02 +0400

    feat: zoom api

--- src/main/java/com/adil/bridgespero/BridgeSperoApplication.java
+++ src/main/java/com/adil/bridgespero/BridgeSperoApplication.java
@@ -1,13 +1,18 @@
 package com.adil.bridgespero;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BridgeSperoApplication {
 
+    @Value("${zoom.client-id}")
+    private static String zoomClientId;
+
     public static void main(String[] args) {
         SpringApplication.run(BridgeSperoApplication.class, args);
+        System.out.println(zoomClientId);
     }
 
 }
