1a818917035abfb9f164096c3928c9b5fb69198a
Adil İsmayılov <ismayilovadil670@gmail.com>
2025-11-24 22:23:56 +0400

    fix: schedule path

--- src/main/java/com/adil/bridgespero/controller/GroupController.java
+++ src/main/java/com/adil/bridgespero/controller/GroupController.java
@@ -21,8 +21,8 @@ import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -86,7 +86,7 @@ public class GroupController {
         return ResponseEntity.noContent().build();
     }
 
-    @PatchMapping("/schedule/{id}")
+    @DeleteMapping("/schedule/{id}")
     public ResponseEntity<Void> deleteSchedule(
             @PathVariable Long id
     ) {
@@ -109,7 +109,7 @@ public class GroupController {
         return ResponseEntity.status(CREATED).body(groupService.createSyllabus(id, request));
     }
 
-    @PatchMapping("/{id}/syllabus")
+    @DeleteMapping("/{id}/syllabus")
     public ResponseEntity<Void> deleteSyllabus(@PathVariable Long id) {
         groupService.deleteSyllabus(id);
         return ResponseEntity.noContent().build();
