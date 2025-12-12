fe3d8139e4e18a596a5229f00455808853a8cee0
Adil İsmayılov <ismayilovadil670@gmail.com>
2025-12-12 15:39:12 +0400

    fix: group start date

--- src/main/java/com/adil/bridgespero/domain/model/dto/request/GroupCreateRequest.java
+++ src/main/java/com/adil/bridgespero/domain/model/dto/request/GroupCreateRequest.java
@@ -2,11 +2,14 @@ package com.adil.bridgespero.domain.model.dto.request;
 
 import com.adil.bridgespero.domain.model.enums.Language;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDate;
 
 public record GroupCreateRequest(
 
@@ -19,9 +22,10 @@ public record GroupCreateRequest(
         @NotNull(message = "Language is missing")
         Language language,
 
-        @NotBlank(message = "Start date is missing")
-        @Schema(pattern = "dd.MM.yyyy")
-        String startDate,
+        @NotNull(message = "Start date is missing")
+        @Schema(pattern = "YYYY-MM-DD")
+        @FutureOrPresent(message = "Start date must not be in the past")
+        LocalDate startDate,
 
         @Positive(message = "Duration must be positive")
         Integer durationInMonths,
