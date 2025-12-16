83af452f8a83c89cc71c9a8719cce4856004a8ad
Adil İsmayılov <ismayilovadil670@gmail.com>
2025-12-16 19:10:16 +0400

    feat: cookie check for token

--- src/main/java/com/adil/bridgespero/constant/CommonConstant.java
+++ src/main/java/com/adil/bridgespero/constant/CommonConstant.java
@@ -8,12 +8,10 @@ public final class CommonConstant {
 
     @NoArgsConstructor(access = AccessLevel.PRIVATE)
     public static final class HttpAttribute {
-        public static final String REQUEST_ID = "request_id";
-        public static final String ELAPSED_TIME = "elapsed_time";
-        public static final String X_FORWARDED_FOR = "X-Forwarded-For";
         public static final String AUTHORIZATION = "Authorization";
         public static final String BEARER = "Bearer ";
         public static final String SET_COOKIE = "Set-Cookie";
+        public static final String ACCESS_TOKEN = "access_token";
     }
 
 }
--- src/main/java/com/adil/bridgespero/util/SecurityUtil.java
+++ src/main/java/com/adil/bridgespero/util/SecurityUtil.java
@@ -1,6 +1,7 @@
 package com.adil.bridgespero.util;
 
 import com.adil.bridgespero.constant.CommonConstant;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -10,10 +11,23 @@ import org.springframework.util.StringUtils;
 public final class SecurityUtil {
 
     public static String resolveToken(HttpServletRequest request) {
+
         String authHeader = request.getHeader(CommonConstant.HttpAttribute.AUTHORIZATION);
-        if (StringUtils.hasText(authHeader) && authHeader.startsWith(CommonConstant.HttpAttribute.BEARER))
+        if (StringUtils.hasText(authHeader)
+            && authHeader.startsWith(CommonConstant.HttpAttribute.BEARER)) {
             return authHeader.substring(CommonConstant.HttpAttribute.BEARER.length());
+        }
+
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (CommonConstant.HttpAttribute.ACCESS_TOKEN.equals(cookie.getName())
+                    && StringUtils.hasText(cookie.getValue())) {
+                    return cookie.getValue();
+                }
+            }
+        }
+
         return null;
     }
-
 }
